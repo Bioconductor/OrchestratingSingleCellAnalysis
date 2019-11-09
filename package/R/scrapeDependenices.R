058bb8ac171e4253169524c279365443b665ccad
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-11-09 11:53:44 -0800

    Docfix and reoxygenated, added packages to Suggests.

--- package/R/scrapeDependenices.R
+++ package/R/scrapeDependenices.R
@@ -1,13 +1,14 @@
 #' Scrape dependencies
 #' 
-#' Scrap Rmarkdown reports in the book for all required dependencies.
+#' Scrape Rmarkdown reports in the book for all required dependencies.
 #'
 #' @param dir String specifying the directory containing Rmarkdown reports.
 #'
 #' @return Character vector of required packages.
 #'
 #' @details
-#' The output of this should be added to the Suggests field of this package.
+#' The output of this should be added to the \code{Suggests} field of \pkg{OSCAUtils}, 
+#' to make it easier to simply install all dependencies required for the book.
 #'
 #' @author Aaron Lun
 #'
