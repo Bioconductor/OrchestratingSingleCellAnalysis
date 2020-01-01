8d7622acbc32d96aa0f3f5553ececfd6ef358ddd
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-01-01 12:44:32 -0800

    Added utility function to spawn the entire book de-novo.

---
+++ package/R/spawnBook.R
@@ -0,0 +1,38 @@
+#' Set up the book repository
+#'
+#' Initialize or update the repository containing the book contents.
+#'
+#' @param dir String containing the path to the (possibly existing) book repository.
+#'
+#' @return
+#' If \code{dir} does not already exist, a Git repository is created at that location.
+#' The directory is then filled with the book contents.
+#' A \code{NULL} is invisibly returned.
+#'
+#' @details
+#' See \code{\link{.setupBookChapters}} for some reasons why we set up the book like this.
+#'
+#' @author Aaron Lun
+#'
+#' @export 
+spawnBook <- function(dir) {
+    if (absent <- !dir.exists(dir)) {
+        dir.create(dir)
+    }
+
+    wd <- getwd()
+    setwd(dir)
+    on.exit(setwd(wd))
+
+    if (absent) {
+        system2("git", "init")
+    }
+
+    setupBookChapters()
+
+    # Copying over various bookdown required files.
+    others <- list.files("OSCABase", "sundries", full.names=TRUE)
+    file.copy(others, basename(others))
+    
+    invisible(NULL)
+}
